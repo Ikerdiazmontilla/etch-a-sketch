e8a5de176de6542815f9b76f9b15b89725c61d2d
Iker <143113054+Ikerdiazmontilla@users.noreply.github.com>
2023-10-19 10:45:37 -0500

    Make grid responsive to hover, put prompt option
    Make the grid change color when you hover,
    give an option to choose the number of squares that you want in
    the grid.

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const gridContainer = document.querySelector(".grid-container")
 
-for(let i = 0; i < 256;i++){
-    const gridDiv = document.createElement("div")
-    gridDiv.className = "grid-div"
-    gridDiv.textContent = "a"
-    gridContainer.appendChild(gridDiv)
+function changeGrid(nSquares = 16){
+    gridContainer.innerHTML = null
+for(let i = 0; i < nSquares**2;i++){
+    const div = document.createElement("div")
+    div.className = "grid-div"
+    flexBasis = (100/nSquares)
+    div.style.flexBasis = `${flexBasis}%`
+    gridContainer.appendChild(div)
 }
+}
+changeGrid()
+
+const gridPrompt = document.querySelector(".grid-prompt")
+gridPrompt.addEventListener("click", () => {
+    while (true){
+        nSquares = parseInt(prompt("How many squares wide do you want your grid to be?(max = 100): "))
+        if (nSquares <= 100){
+            changeGrid(nSquares)
+            break;
+        }
+        else{
+            continue;
+        }
+    }
+} )
